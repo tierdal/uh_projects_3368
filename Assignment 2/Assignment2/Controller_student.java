7c7050ff35840026e5aad49b2a5c8a84ecf578aa
tierdal <tierdal@hotmail.com>
2019-10-07 18:23:32 -0500

    added main filter algorithm

--- Assignment 2/Assignment2/Controller_student.java
+++ Assignment 2/Assignment2/Controller_student.java
@@ -1,6 +1,8 @@
 package Assignment2;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
@@ -15,7 +17,9 @@ public class Controller_student {
 
     @FXML public TableColumn col_1,col_2,col_3,col_4,col_5;
     @FXML TableView student_tableview;
-    @FXML public JFXComboBox combo_majors;
+    @FXML public JFXComboBox combo_majors,combo_age_from,combo_age_to;
+    @FXML public JFXTextField text_gpa_from,text_gpa_to;
+    @FXML public JFXButton button_exit,button_validate;
 
     private ObservableList<Students> student_data;
 
@@ -24,6 +28,7 @@ public class Controller_student {
 
         populateDataTable();
         populateMajorList();
+        populateAgeList();
 
     }
 
@@ -40,7 +45,6 @@ public class Controller_student {
         }
         return conn;
     }
-
 
     private void populateDataTable() {
         student_data = FXCollections.observableArrayList();
@@ -65,21 +69,82 @@ public class Controller_student {
 
     private void populateMajorList() {
         Connection conn = this.connect_db();
-        ObservableList<String> combo_options = FXCollections.observableArrayList();
+        ObservableList<String> combo_majors_list = FXCollections.observableArrayList();
         combo_majors.getItems().clear();
         try {
             String sql = "SELECT Major FROM majors Order By Major";
             ResultSet result_set = conn.createStatement().executeQuery(sql);
 
             while (result_set.next()) {
-                combo_options.add(result_set.getString("Major"));
+                combo_majors_list.add(result_set.getString("Major"));
             }
-            combo_majors.setItems(combo_options);
+            combo_majors.setItems(combo_majors_list);
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Error on Building Data");
         }
+    }
+
+    private void populateAgeList(){
+        ObservableList<Integer> combo_age_list = FXCollections.observableArrayList();
+        for(int counter=18;counter<100;counter++){
+            combo_age_list.add(counter);
+        }
+        combo_age_from.setItems(combo_age_list);
+        combo_age_to.setItems(combo_age_list);
+    }
+
+    @FXML public void updateDatatable(){
+        boolean validated_age = validateAge();
+        boolean validate_gpa = validateGPA();
+
+        if (validate_gpa && validated_age) {
+            student_data = null;
+            student_data = FXCollections.observableArrayList();
+            int age_from = Integer.parseInt(String.valueOf(combo_age_from.getValue()));
+            int age_to = Integer.parseInt(String.valueOf(combo_age_to.getValue()));
+            double gpa_from = Double.parseDouble(String.valueOf(text_gpa_from.getText()));
+            double gpa_to = Double.parseDouble(String.valueOf(text_gpa_to.getText()));
+            String major;
+
+            if (combo_majors.getValue() == null){
+                major = "";
+            } else {
+                major = " AND major LIKE " + String.valueOf(combo_majors.getValue());
+            }
+
+            Connection conn = this.connect_db();
+            String sql_main = "SELECT * FROM students WHERE age >= " + age_from + " AND age <= " + age_to + " AND gpa >= " + gpa_from + " AND gpa <= " + gpa_to + major + " ORDER BY id";
+            System.out.println(sql_main);
+            try {
+                PreparedStatement preparedStatement = conn.prepareStatement(sql_main);
+                ResultSet result_set = preparedStatement.executeQuery();
+                while (result_set.next()) {
+                    student_data.add(new Students(result_set.getString(1),result_set.getString(2),result_set.getString(3),result_set.getString(4),result_set.getString(5)));
+                }
+                student_tableview.setItems(student_data);
+
+                result_set.close();
+                conn.close();
+            } catch (SQLException tableQueryException) {
+                System.err.println(tableQueryException.toString());
+            }
+        }
+    }
+
+    private boolean validateAge(){
+
+        return true;
+    }
+
+    private boolean validateGPA(){
+
+        return true;
+    }
+
+    private void clearFilters(){
+
     }
 
     public class Students{
