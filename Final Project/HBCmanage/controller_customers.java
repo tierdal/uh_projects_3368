7c2e1a2bc8e29933b59221a22d8fd2ae45b63656
iriny <irinya@gmail.com>
2019-11-15 18:23:59 -0600

    Added connections to other controllers

--- Final Project/HBCmanage/controller_customers.java
+++ Final Project/HBCmanage/controller_customers.java
@@ -1,4 +1,23 @@
 package HBCmanage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class controller_customers {
+
+    //Connect to remote MySQL DB
+    private Connection connect() {
+        String url = "jdbc:mysql://db4free.net/uh2336";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            conn.setAutoCommit(false);
+            System.out.println("You should have remembered this processes better :(");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
 }
