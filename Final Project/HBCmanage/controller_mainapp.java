4437be48591b00eb77d27b30273eef283750ed62
Egor Shumeyko <tierdal@hotmail.com>
2019-11-15 18:22:52 -0600

    add button actions

--- Final Project/HBCmanage/controller_mainapp.java
+++ Final Project/HBCmanage/controller_mainapp.java
@@ -1,4 +1,24 @@
 package HBCmanage;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class controller_mainapp {
+    @FXML JFXButton button_customers;
+    @FXML JFXButton button_inventory;
+    @FXML JFXButton button_salesorder;
+
+    @FXML public void btn_customers_action() throws IOException {
+
+    }
+
+    @FXML public void btn_inventory_action() throws IOException {
+
+    }
+
+    @FXML public void btn_salesorders_action() throws IOException {
+
+    }
 }
