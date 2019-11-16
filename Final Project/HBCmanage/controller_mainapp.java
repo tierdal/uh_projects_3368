fa97d0ae946548b1475e5049cfca6a7e4d83dfc7
Egor Shumeyko <tierdal@hotmail.com>
2019-11-15 18:38:59 -0600

    add controls for main form buttons and added proper fxml and controllers

--- Final Project/HBCmanage/controller_mainapp.java
+++ Final Project/HBCmanage/controller_mainapp.java
@@ -2,6 +2,10 @@ package HBCmanage;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -10,15 +14,27 @@ public class controller_mainapp {
     @FXML JFXButton button_inventory;
     @FXML JFXButton button_salesorder;
 
-    @FXML public void btn_customers_action() throws IOException {
 
-    }
-
-    @FXML public void btn_inventory_action() throws IOException {
-
-    }
 
     @FXML public void btn_salesorders_action() throws IOException {
-
+        Stage orderStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("form_orders.fxml"));
+        orderStage.setTitle("HBC Manage - Orders");
+        orderStage.setScene(new Scene(root, 1200, 600));
+        orderStage.show();
+    }
+    @FXML public void btn_inventory_action() throws IOException {
+        Stage inventoryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("form_inventory.fxml"));
+        inventoryStage.setTitle("HBC Manage - Inventory");
+        inventoryStage.setScene(new Scene(root, 1200, 600));
+        inventoryStage.show();
+    }
+    @FXML public void btn_customers_action() throws IOException {
+        Stage customerStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("form_customers.fxml"));
+        customerStage.setTitle("HBC Manage - Customers");
+        customerStage.setScene(new Scene(root, 600, 400));
+        customerStage.show();
     }
 }
