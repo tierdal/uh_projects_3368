343bc102e9ce4377ccfcbe66879c6906fa282b4a
Egor Shumeyko <tierdal@hotmail.com>
2019-11-16 14:42:41 -0600

    add customer form fields, buttons, and some code

---
+++ Final Project/HBCmanage/class_global_vars.java
@@ -0,0 +1,8 @@
+package HBCmanage;
+
+public class class_global_vars {
+    public static String db_url = "db4free.net";
+    public static String db_user = "eshumeyko";
+    public static String db_pass = "Th1sGuyF@wks";
+    public static String db_database = "uh2336";
+}
